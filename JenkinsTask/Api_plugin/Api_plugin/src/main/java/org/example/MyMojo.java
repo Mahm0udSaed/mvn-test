7e4a0fe4ba0267adfdd440190c499a4f6e054249
Mahm0udSaed <mahmoudsaltah145@gmail.com>
2020-11-09 14:21:14 +0200

    initial

--- JenkinsTask/Api_plugin/Api_plugin/src/main/java/org/example/MyMojo.java
+++ JenkinsTask/Api_plugin/Api_plugin/src/main/java/org/example/MyMojo.java
@@ -1,23 +1,10 @@
 package org.example;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import javax.ws.rs.client.ClientBuilder;
@@ -38,6 +25,7 @@ import java.net.URI;
  * 
  * @phase process-sources
  */
+@Mojo( name = "rest-request" )
 public class MyMojo
     extends AbstractMojo
 {
@@ -58,14 +46,7 @@ public class MyMojo
     @Parameter
     private String requestType = MediaType.APPLICATION_JSON;
 
-    /**
-     * The type of the data returned by the REST request.
-     *
-     * The expected data type of the REST response. Default
-     * <code>MediaType.APPLICATION_OCTET_STREAM_TYPE</code>
-     *
-     * See <code>requestType</code> for example of usage.
-     */
+
     @Parameter
     private String responseType = MediaType.APPLICATION_JSON;
 
@@ -149,17 +130,13 @@ public class MyMojo
         return endpoint;
     }
 
-    /**
-     * @return the resource
-     */
+
     public String getResource()
     {
         return resource;
     }
 
-    /**
-     * @return the filesets
-     */
+
 //    public List<FileSet> getFilesets()
 //    {
 //        return filesets;
